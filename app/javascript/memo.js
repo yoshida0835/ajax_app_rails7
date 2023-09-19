e195e9c4e33f6977f79e0455c16938f6e5727801
yoshida0835 <141699753+yoshida0835@users.noreply.github.com>
2023-09-20 00:05:31 +0900

    コントローラーにリクエストを送信する

---
+++ app/javascript/memo.js
@@ -0,0 +1,12 @@
+function post (){
+  const form = document.getElementById("form");
+  form.addEventListener("submit", () => {
+    const formDate = new FormDate(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);]
+    XHR.responseType = "json";
+    XHR.send(formData);
+  })
+};
+
+window.addEventListener('turbo:load', post);
