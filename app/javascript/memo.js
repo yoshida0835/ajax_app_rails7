550ad64882d3f39e426d4ec0e77a62a9a92d2098
yoshida0835 <141699753+yoshida0835@users.noreply.github.com>
2023-09-20 00:47:17 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -3,7 +3,7 @@ function post (){
   form.addEventListener("submit", () => {
     const formDate = new FormDate(form);
     const XHR = new XMLHttpRequest();
-    XHR.open("POST", "/posts", true);]
+    XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
   })
